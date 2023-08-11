3215f8ea2b9484880c4b1e8328d9a09fe941890d
Gregory <gsukanto@amazon.com>
2023-08-11 10:25:40 +0700

    feat: add amplify library

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {ThemeProvider} from "@aws-amplify/ui-react";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={studioTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
